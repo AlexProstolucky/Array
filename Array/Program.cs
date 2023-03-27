d127f62dd80929619975b083e71b307a5e0769c8
AlexProstolucky <alexprostolucky@gmail.com>
2023-03-27 18:42:11 +0300

    Добавьте файлы проекта.

---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, -1, 0, 2, 3, -4, 5, 4, -8, 7, 11 };
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < 0) Console.Write($"{arr[i]} ");
+        }
+        Console.WriteLine("");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > 0) Console.Write($"{arr[i]} ");
+        }
+    }
+}
